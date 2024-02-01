0f79ae68ad7185911daecd2532a44b61418ef61c
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2024-02-01 10:46:03 +0100

    added Video class

--- src/main/java/de/neuefische/VideoPlayer.java
+++ src/main/java/de/neuefische/VideoPlayer.java
@@ -3,9 +3,20 @@ package de.neuefische;
 // Step 3: Create a class 'VideoPlayer' that also implements the 'Playable' interface. Implement the 'play' method to play a video.
 
 public class VideoPlayer implements Playable{
+    Video video = null;
+
 
     @Override
     public void play() {
         System.out.println("playing Video!");
+    }
+
+    public void ejectVideo(){
+        if(video.isNull()){
+            System.out.println("There is no Video to eject!");
+        }
+
+
+        video = null;
     }
 }
